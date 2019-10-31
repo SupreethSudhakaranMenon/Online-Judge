dcbfe20f1f050dfbd6a153d94eda5ed8e3267f04
psancheti110 <54016434+psancheti110@users.noreply.github.com>
2019-10-31 11:21:56 +0530

    Solution for Line Segment Intersection problem

--- main.c
+++ main.c
@@ -1,25 +1,88 @@
-#include <stdio.h>
-#include <math.h>
+#include<stdio.h>
+#include<math.h>
 
 int main (void)
-{long long int T,i;
-   double C;
+{
+    long long int T,x1,x2,x3,x4,y1,y2,y3,y4,i;
+    double m1,m2,Y,X;
     scanf("%lld", &T);
     for(i=0;i<T;i++)
     {
-        scanf("%lf", &C);
-    double dy,y,x=sqrt(C);
-    y = pow(x,2)+sqrt(x)-C;
-    dy = 2*x + 1/(2*sqrt(x));
+    scanf("%lld %lld %lld %lld %lld %lld %lld %lld", &x1, &y1, &x2, &y2, &x3, &y3, &x4, &y4);
+    
+    if (x1==x2 && x3!=x4)
     {
-        while (fabs (y) > 0.000001)
+        printf("*");
+        m2 = (y4-y3)/(x4-x3);
+        Y = y3 + m2*(x1-x3);
+        
+        if (fmax(y1,y2)>=Y && Y>=fmin(y1,y2) && fmax(y3,y4)>=Y && Y>=fmin(y3,y4))
+            printf("Yes\n");
+        else printf("No\n");
+    }
+    
+    else if (x1!=x2 && x3==x4)
         {
-            x = x - y/dy;
-        y = pow(x,2)+sqrt(x)-C;
-        dy = 2*x + 1/(2*sqrt(x));
+            
+            printf("**");
+        
+        m1 = (y2-y1)/(x2-x1);
+        Y = y1 + m1*(x3-x1);
+        
+        if (fmax(y3,y4)>=Y && Y>=fmin(y3,y4) && fmax(y1,y2)>=Y && Y>=fmin(y1,y2))
+            printf("Yes\n");
+        else printf("No\n");
+    }
+    
+    else if (x1==x2 && x3==x4)
+     {
+        
+         printf("***");
+         if (x1!=x3)
+         {
+              printf("****");
+             printf("No\n");
+         }
+         else if (x1==x3)
+         {
+              printf("*****");
+            if (fmax(y1,y2)>=y3 && y3>=fmin(y1,y2))
+                printf("Yes\n");
+            else if (fmax(y1,y2)>=y4 && y4>=fmin(y1,y2))
+                printf("Yes\n");
+            else if (fmax(y3,y4)>=y1 && y1>=fmin(y3,y4))
+                printf("Yes\n");
+            else if (fmax(y3,y4)>=y2 && y2>=fmin(y3,y4))
+                printf("Yes\n");
+            else printf("No\n");
+         }
+     }
+    else
+    {
+       
+      printf("******");
+        if(fabs(m1-m2)!=0)
+        {
+        X= ((y3-y1)+(m1*x1-m2*x2))/(m1-m2);
+            if (fmax(x1,x2)>=X && X>=fmin(x1,x2) && fmax(x3,x4)>=X && X>=fmin(x3,x4) )
+                printf("Yes\n");
+            else printf("No\n");
         }
+        else if (fabs(m1-m2)==0)
+        {
+             printf("*******");
+            if (fmax(y1,y2)>=y3 && y3>=fmin(y1,y2))
+                printf("Yes\n");
+            else if (fmax(y1,y2)>=y4 && y4>=fmin(y1,y2))
+                printf("Yes\n");
+            else if (fmax(y3,y4)>=y1 && y1>=fmin(y3,y4))
+                printf("Yes\n");
+            else if (fmax(y3,y4)>=y2 && y2>=fmin(y3,y4))
+                printf("Yes\n");
+            else printf("No\n");
+        }
+        
     }
-        printf("%.6lf\n", x);
     }
-    return 0;
+     return 0;
 }
